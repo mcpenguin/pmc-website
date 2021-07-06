06a0ac230224925888ade3d447c3e152c42a0a59
mcpenguin <marcus.chanwc@gmail.com>
2021-07-07 01:48:15 +0800

    added gradients to backgrounds

--- src/components/cover/cover.js
+++ src/components/cover/cover.js
@@ -19,18 +19,40 @@ export default class Cover extends Component {
                         <img src={combo} />
                         <div className="description combo-description">
                             <h2>Combinatorics & Optimization</h2>
+                            <p>
+                                Combinatorics is an area of mathematics primarily concerned with counting, 
+                                both as a means and an end in obtaining results, and certain 
+                                properties of finite structures.
+                            </p>
+                            <p>
+                                Mathematical optimization, or mathematical programming, is the selection of a best element, 
+                                with regard to some criterion, from some set of available alternatives.
+                            </p>
                         </div>
                     </div>
                     <div className='cover-image'>
                         <img src={applied_math} />
                         <div className="description applied-math-description">
                             <h2>Applied Mathematics</h2>
+                            <p>
+                                Applied mathematics is the application of mathematical methods by different
+                                fields such as physics, engineering, medicine, biology, finance, business,
+                                computer science, and industry. Thus, applied mathematics is a combination
+                                of mathematical science and specialized knowledge.
+                            </p>
                         </div>
                     </div>
                     <div className='cover-image'>
                         <img src={pure_math} />
                         <div className="description pure-math-description">
                             <h2>Pure Mathematics</h2>
+                            <p>
+                                Pure mathematics is the study of mathematical concepts independently of any application
+                                outside mathematics. These concepts may originate in real-world concerns,
+                                and the results obtained may later turn out to be useful for practical
+                                applications, but pure mathematicians are not primarily motivated by
+                                such applications.
+                            </p>
                         </div>
                     </div>
                 </div>
