ce9bed00a45874ceda9362ad7e1b53ad4464abd5
mcpenguin <marcus.chanwc@gmail.com>
2021-07-06 16:44:56 +0800

    imported make section folder helper

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,15 @@
+// React component class for navbar section
+import React, { Component } from 'react';
+
+// import stylesheet
+import './navbar.scss';
+
+export default class navbar extends Component {
+    render() { 
+        return (
+            <section id='#navbar'>
+            
+            </>
+        ); 
+    }
+}
