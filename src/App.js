5ed3ef0cb35ab240ca24a22e186b051ca0c130bd
mcpenguin <marcus.chanwc@gmail.com>
2021-07-06 16:55:47 +0800

    added pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,78 @@
-import { Component } from 'react'
+// main React component for website
 
-export default class App extends Component {
+import React, { Component } from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from 'react-router-dom';
+
+import TopNavbar from './components/navbar/navbar';
+
+class HomePage extends Component {
     render() {
         return (
-            <p>Hello World!</p>
+            <>
+                <TopNavbar />
+            </>
+        )
+    }
+}
+
+class EventsPage extends Component {
+    render() {
+        return (
+            <>
+                <TopNavbar />
+            </>
+        )
+    }
+}
+
+class POWPage extends Component {
+    render() {
+        return (
+            <>
+                <TopNavbar />
+            </>
+        )
+    }
+}
+
+class ConstitutionPage extends Component {
+    render() {
+        return (
+            <>
+                <TopNavbar />
+            </>
+        )
+    }
+}
+
+class ContactUsPage extends Component {
+    render() {
+        return (
+            <>
+                <TopNavbar />
+            </>
+        )
+    }
+}
+
+export default class App extends Component {
+
+    render() {
+        return (
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={HomePage} />
+                    <Route exact path="/events" component={EventsPage} />
+                    <Route exact path="/problems-of-the-week" component={POWPage} />
+                    <Route exact path="/constitution" component={ConstitutionPage} />
+                    <Route exact path="/contact-us" component={ContactUsPage} />
+                </Switch>
+            </Router>
         );
     }
+
 }
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,15 +1,51 @@
 // React component class for navbar section
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+// import logo
+import logo from './pmamco.png';
 
 // import stylesheet
 import './navbar.scss';
 
-export default class navbar extends Component {
-    render() { 
+class NavbarLink extends Component {
+    render() {
+        const str = `#${this.props.id}`;
         return (
-            <section id='#navbar'>
-            
-            </>
-        ); 
+            <Nav.Item>
+                <Link to={`/${this.props.id}`} className="nav-item">
+                    {this.props.title}
+                </Link>
+            </Nav.Item>
+        )
+    }
+}
+
+export default class TopNavbar extends Component {
+
+    render() {
+        return (
+            <section className="section-header">
+                <Navbar expand="lg" variant="dark">
+                    <Navbar.Brand href="/" className="navbar-brand">
+                        <img src={logo} id="navbar-img" /> The Arctic Blog
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="my-navbar" />
+                    <Navbar.Collapse>
+                        <Nav id="my-navbar" className="ml-auto">
+                            <NavbarLink id="projects" title="Projects" />
+                            <NavbarLink id="experience" title="Experience" />
+                            <NavbarLink id="education" title="Education" />
+                            <NavbarLink id="achievements" title="Achievements" />
+                            <NavbarLink id="notes" title="Notes" />
+                            <NavbarLink id="credits" title="Credits" />
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+            </section>
+        )
     }
 }
