ec99e50000c3bf264b13dc5156d4c4abb36b4ca0
mcpenguin <marcus.chanwc@gmail.com>
2021-07-07 01:02:40 +0800

    added divs for description

--- src/components/cover/cover.js
+++ src/components/cover/cover.js
@@ -17,12 +17,21 @@ export default class Cover extends Component {
                 <div>
                     <div className='cover-image'>
                         <img src={combo} />
+                        <div className="description combo-description">
+                            <p>Combinatorics & Optimization</p>
+                        </div>
                     </div>
                     <div className='cover-image'>
                         <img src={applied_math} />
+                        <div className="description applied-math-description">
+                            <p>Applied Mathematics</p>
+                        </div>
                     </div>
                     <div className='cover-image'>
                         <img src={pure_math} />
+                        <div className="description pure-math-description">
+                            <p>Pure Mathematics</p>
+                        </div>
                     </div>
                 </div>
             </section>
