9f5e400ff89ff93e606b171d21bdf1eb7c0e891c
mcpenguin <marcus.chanwc@gmail.com>
2021-07-07 23:58:37 +0800

    did contact us animation

--- src/components/contact-us/contact-us.js
+++ src/components/contact-us/contact-us.js
@@ -9,13 +9,40 @@ import './contact-us.scss';
 // - logoIcon: name of font awesome icon for link (eg fas fa-address-book)
 // - desc: "description" of the link (eg room number, link to discord etc)
 class ContactMeIcon extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isElementVisible: false,
+        }
+    }
+
+    // get intersection observer to see whether element is in view
+    observer = new IntersectionObserver(
+        (entries, observer) => {
+            entries.forEach(entry => {
+                this.setState({
+                    isElementVisible: entry.isIntersecting,
+                })
+            })
+        },
+        {
+            rootMargin: '0px',
+            threshold: 0.8,
+        }
+    )
+
+    componentDidMount() {
+        this.observer.observe(document.querySelector('#contact-us'));
+    }
+
     render() {
         return (
             <div className='logo'>
-                <div className='logo-icon'>
+                <div className={`logo-icon ${this.state.isElementVisible ? 'start-logo-icon-animation' : ''}`}>
                     <i className={this.props.logoIcon}></i>
                 </div>
-                <div className='logo-link'>
+                <div className={`logo-link ${this.state.isElementVisible ? 'start-logo-link-icon-animation' : ''}`}>
                     {this.props.desc}
                 </div>
             </div>
@@ -24,9 +51,12 @@ class ContactMeIcon extends Component {
 }
 
 export default class ContactUs extends Component {
+
     render() {
         return (
             <section id='contact-us'>
+                <div className="red-line"></div>
+                <div className="orange-line"></div>
                 <h2>Contact Us</h2>
                 <div className='logo-container'>
                     <ContactMeIcon
